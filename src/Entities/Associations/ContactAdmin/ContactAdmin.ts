3c863a0cda0f8c7c0ee10961be763181aeb4457f
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-03-08 22:28:25 -0300

    feat(contactAdmin): add table associative to contact and admin

---
+++ src/Entities/Associations/ContactAdmin/ContactAdmin.ts
@@ -0,0 +1,15 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Admin } from "../../Admin/Admin";
+import { Contact } from "../../Contact/Contact";
+
+@Entity("ContactAdmin")
+export class ContactAdmin {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @ManyToOne(() => Admin, (a) => a.contacts, {nullable: false})
+    admin!: Admin;
+
+    @ManyToOne(() => Contact, (c) => c.contactAdmins, {nullable: false})
+    contact!: Contact
+}
