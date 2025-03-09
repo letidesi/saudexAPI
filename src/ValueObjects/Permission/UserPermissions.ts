b7323ed9bc2093ae4463b29e48f7fa5d49f1ba51
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-03-09 15:58:10 -0300

    feat(permission): add list permission of user permission

---
+++ src/ValueObjects/Permission/UserPermissions.ts
@@ -0,0 +1,8 @@
+import { PermissionType } from "./Type/PermissionTypeEnum";
+
+export const userPermissions: PermissionType[] = [
+    PermissionType.viewMedicalCenter,
+    PermissionType.viewUser,
+    PermissionType.updateOwnUser,
+    PermissionType.deleteOwnUser,
+];
