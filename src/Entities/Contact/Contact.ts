85dffd7ed7d8167148b4b8eaaf424bf57a8575f2
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-03-08 22:27:43 -0300

    feat(contact): add entity to contact

---
+++ src/Entities/Contact/Contact.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { ContactAdmin } from "../Associations/ContactAdmin/ContactAdmin";
+import { ContactMedicalCenter } from "../Associations/ContactMedicalCenter/ContactMedicalCenter";
+
+@Entity("Contact")
+export class Contact {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ type: "nvarchar", length: 50, nullable: true })
+    phone?: string;
+
+    @Column({ type: "nvarchar", length: 100, nullable: false })
+    email!: string;
+
+    @OneToMany(() => ContactAdmin, (c) => c.contact, { cascade: true })
+    contactAdmins!: ContactAdmin[];
+
+    @OneToMany(() => ContactMedicalCenter, (c) => c.contact, { cascade: true }) 
+    contactMedicalCenters!: ContactMedicalCenter[];
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date
+
+    @DeleteDateColumn()
+    deletedAt!: Date
+}
