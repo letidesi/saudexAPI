63e642e19f67ec65d3bb255e1830720de422809c
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-03-09 14:30:54 -0300

    feat(role): add new entity role

---
+++ src/Entities/Role/Role.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { RoleType } from "../../ValueObjects/Role/RoleEnum";
+import { UserRole } from "../Associations/UserRole/UserRole";
+
+@Entity("Role")
+export class Role {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ type: "nvarchar", length: 100, unique: true, nullable: false})
+    type!: RoleType;
+
+    @OneToMany(() => UserRole, (u) => u.role, {cascade: true})
+    users!: UserRole[];
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+    @DeleteDateColumn()
+    deletedAt!: Date;
+}
