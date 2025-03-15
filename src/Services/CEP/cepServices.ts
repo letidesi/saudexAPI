aeeeea79b1a786657656ff21138d83cc638eeb0e
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-03-15 20:40:26 -0300

    feat(services): add function to get cep

---
+++ src/Services/CEP/cepServices.ts
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+
+export async function GetAddressByCep(cep: string) {
+    try {
+        const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const data = (await res.json() as ViaCepResponse);
+
+        if (data.erro) throw new Error("CEP not found.")
+
+        return {
+            street: data.logradouro,
+            neighborhod: data.bairro,
+            city: data.localidade,
+            state: data.uf,
+            cep: cep
+        }
+
+    } catch (error) {
+        throw new Error("Error in search cep.")
+    }
+}
