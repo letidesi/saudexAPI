fa06a0304166f16697d140bbbc4ac2536f609722
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 13:30:04 -0300

    refactor(permission): add comments

--- src/Entities/Permission/Permission.ts
+++ src/Entities/Permission/Permission.ts
@@ -2,23 +2,32 @@ import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryG
 import { PermissionType } from "../../ValueObjects/Permission/Type/PermissionTypeEnum";
 import { RolePermission } from "../Associations/RolePermission/RolePermission";
 
-@Entity("Permission")
+@Entity("Permission", {
+    comment: "Tabela que armazena os tipos de permissões disponíveis no sistema."
+})
 export class Permission {
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn("uuid", {
+        comment: "Identificador único da permissão (UUID)."
+    })
     id!: string;
 
-    @Column({ type: "nvarchar", length: 100, unique: true, nullable: false })
-    type!: PermissionType
+    @Column({
+        type: "nvarchar",
+        length: 100, unique: true,
+        nullable: false,
+        comment: "Tipo de permissão."
+    })
+    type!: PermissionType;
 
     @OneToMany(() => RolePermission, (r) => r.permission, { cascade: true })
     roles!: RolePermission[];
 
-    @CreateDateColumn()
+    @CreateDateColumn({ comment: "Data de criação do registro." })
     createdAt!: Date;
 
-    @UpdateDateColumn()
+    @UpdateDateColumn({ comment: "Data da última atualização do registro." })
     updatedAt!: Date;
 
-    @DeleteDateColumn()
+    @DeleteDateColumn({ comment: "Data de exclusão lógica (soft delete)." })
     deletedAt!: Date;
 }
