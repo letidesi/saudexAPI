088acad39a899c87b1211ed44ddf33fb5b9b6786
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 13:22:03 -0300

    refactor(medication): add comments

--- src/Entities/Medication/Medication.ts
+++ src/Entities/Medication/Medication.ts
@@ -2,21 +2,35 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { MedicalCenter } from "../MedicalCenter/MedicalCenter";
 import { MedicationType } from "../../ValueObjects/Medication/MedicationEnum";
 
-
-@Entity("Medications")
+@Entity("Medications", {
+    comment: "Tabela que armazena os medicamentos cadastrados nos centros médicos."
+})
 export class Medication {
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn("uuid", {
+        comment: "Identificador único do medicamento (UUID)."
+    })
     id!: string;
 
     @ManyToOne(() => MedicalCenter, (medicalCenter) => medicalCenter.medications, { nullable: false, cascade: true })
     medicalCenter!: MedicalCenter;
 
-    @Column({ type: "enum", enum: MedicationType, nullable: false })
+    @Column({ 
+        type: "enum", 
+        enum: MedicationType, 
+        nullable: false, 
+        comment: "Tipo de medicamento (ex: Vacina, Medicamentos e Insulina)." })
     type!: MedicationType;
 
-    @Column({ type: "nvarchar", length: 255, nullable: false })
+    @Column({ 
+        type: "nvarchar", 
+        length: 255, nullable: false, 
+        comment: "Nome do medicamento." })
     name!: string;
 
-    @Column({ type: "int", nullable: false, default: 0 })
+    @Column({ 
+        type: "int", 
+        nullable: false, 
+        default: 0, 
+        comment: "Quantidade disponível do medicamento no centro médico." })
     quantity!: number;
 }
