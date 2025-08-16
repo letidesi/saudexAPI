d66e2b6a8eff0dcc58f395a87a98a43bdc3fc5eb
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 14:24:31 -0300

    refactor(role): add extends

--- src/Entities/Role/Role.ts
+++ src/Entities/Role/Role.ts
@@ -1,26 +1,18 @@
 import {
   Column,
-  CreateDateColumn,
-  DeleteDateColumn,
   Entity,
   OneToMany,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
 } from 'typeorm';
 import { RoleType } from '../../ValueObjects/Role/RoleEnum';
 import { UserRole } from '../Associations/UserRole/UserRole';
 import { RolePermission } from '../Associations/RolePermission/RolePermission';
+import { EntityWithTimestamps } from '../../Helpers/EntityWithTimestamps';
 
 @Entity('Role', {
   comment:
     'Tabela que armazena os papéis (roles) do sistema, como Administrador, Usuário, Convidado, etc.',
 })
-export class Role {
-  @PrimaryGeneratedColumn('uuid', {
-    comment: 'Identificador único do papel (UUID).',
-  })
-  id!: string;
-
+export class Role extends EntityWithTimestamps {
   @Column({
     type: 'nvarchar',
     length: 100,
@@ -36,13 +28,4 @@ export class Role {
 
   @OneToMany(() => RolePermission, (r) => r.role, { cascade: true })
   permissions!: RolePermission[];
-
-  @CreateDateColumn({ comment: 'Data de criação do registro.' })
-  createdAt!: Date;
-
-  @UpdateDateColumn({ comment: 'Data da última atualização do registro.' })
-  updatedAt!: Date;
-
-  @DeleteDateColumn({ comment: 'Data de exclusão.' })
-  deletedAt!: Date;
 }
