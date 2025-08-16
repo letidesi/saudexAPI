fe2ccb1e028589912aafea2b602d9cc6d245bd07
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 12:52:37 -0300

    refactor(admin): add comments

--- src/Entities/Admin/Admin.ts
+++ src/Entities/Admin/Admin.ts
@@ -2,19 +2,35 @@ import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, OneToMan
 import { ContactAdmin } from "../Associations/ContactAdmin/ContactAdmin";
 import { Address } from "../Address/Address";
 
-@Entity("Admin")
+@Entity("Admin", {
+    comment: "Tabela que armazena os administradores do sistema."
+})
 export class Admin {
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn("uuid", {
+        comment: "Identificador único do administrador (UUID)."
+    })
     id!: string;
 
-    @Column({ type: "nvarchar", length: 200, nullable: false })
+    @Column({ 
+        type: "nvarchar", 
+        length: 200, nullable: false, 
+        comment: "Primeiro nome do administrador." 
+    })
     firstName!: string;
 
-    @Column({ type: "nvarchar", length: 200, nullable: false })
+    @Column({ 
+        type: "nvarchar", length: 200, 
+        nullable: false, 
+        comment: "Sobrenome do administrador." 
+    })
     lastName!: string;
 
-    @Column({ type: "nvarchar", length: 255, nullable: false })
-    passwordHash!: string
+    @Column({ 
+        type: "nvarchar", 
+        length: 255, nullable: false, 
+        comment: "Hash da senha do administrador." 
+    })
+    passwordHash!: string;
 
     @OneToMany(() => ContactAdmin, (c) => c.admin, { cascade: true })
     contacts!: ContactAdmin[];
@@ -22,12 +38,12 @@ export class Admin {
     @ManyToOne(() => Address, { nullable: true })
     address?: Address;
 
-    @CreateDateColumn()
+    @CreateDateColumn({ comment: "Data de criação do registro." })
     createdAt!: Date;
 
-    @UpdateDateColumn()
-    updatedAt!: Date
+    @UpdateDateColumn({ comment: "Data da última atualização do registro." })
+    updatedAt!: Date;
 
-    @DeleteDateColumn()
-    deletedAt!: Date
+    @DeleteDateColumn({ comment: "Data de exclusão." })
+    deletedAt!: Date;
 }
