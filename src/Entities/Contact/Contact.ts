f0e585efb6ee3cf2d41ad6ab2f21aa64c45e24d1
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 12:56:01 -0300

    refactor(contact): add comments

--- src/Entities/Contact/Contact.ts
+++ src/Entities/Contact/Contact.ts
@@ -3,15 +3,27 @@ import { ContactAdmin } from "../Associations/ContactAdmin/ContactAdmin";
 import { ContactMedicalCenter } from "../Associations/ContactMedicalCenter/ContactMedicalCenter";
 import { ContactUser } from "../Associations/ContactUser/ContactUser";
 
-@Entity("Contact")
+@Entity("Contact", {
+    comment: "Tabela que armazena os contatos de usuários, administradores e centros médicos."
+})
 export class Contact {
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn("uuid", {
+        comment: "Identificador único do contato (UUID)."
+    })
     id!: string;
 
-    @Column({ type: "nvarchar", length: 50, nullable: true })
+    @Column({
+        type: "nvarchar",
+        length: 50, nullable: true,
+        comment: "Número de telefone/celular do contato."
+    })
     phone?: string;
 
-    @Column({ type: "nvarchar", length: 100, nullable: false })
+    @Column({
+        type: "nvarchar",
+        length: 100, nullable: false,
+        comment: "Endereço de e-mail do contato."
+    })
     email!: string;
 
     @OneToMany(() => ContactAdmin, (c) => c.contact, { cascade: true })
@@ -23,12 +35,12 @@ export class Contact {
     @OneToMany(() => ContactUser, (c) => c.contact, { cascade: true })
     contactUsers!: ContactUser[];
 
-    @CreateDateColumn()
+    @CreateDateColumn({ comment: "Data de criação do registro." })
     createdAt!: Date;
 
-    @UpdateDateColumn()
-    updatedAt!: Date
+    @UpdateDateColumn({ comment: "Data da última atualização do registro." })
+    updatedAt!: Date;
 
-    @DeleteDateColumn()
-    deletedAt!: Date
+    @DeleteDateColumn({ comment: "Data de exclusão do registro." })
+    deletedAt!: Date;
 }
