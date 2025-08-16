253c0929c1a2acfce4268e23bc7b559e7c52dd3d
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-16 13:27:18 -0300

    feat(operatingHour): add comments

--- src/Entities/OperatingHour/Operatinghour.ts
+++ src/Entities/OperatingHour/Operatinghour.ts
@@ -1,30 +1,40 @@
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { MedicalCenter } from "../MedicalCenter/MedicalCenter";
-import { WeekDay } from "../../ValueObjects/OperatingHour/OperatingHour";
+import { WeekDay } from "../../ValueObjects/OperatingHour/OperatingHourEnum";
 
-@Entity("OperatingHour")
 export class OperatingHour {
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn("uuid", {
+        comment: "Identificador único do horário de funcionamento (UUID)."
+    })
     id!: string;
 
     @ManyToOne(() => MedicalCenter, (m) => m.operatingHours, { nullable: false, cascade: true })
     medicalCenter!: MedicalCenter;
 
-    @Column({ type: "nvarchar", length: 100, nullable: false })
+    @Column({ 
+        type: "nvarchar", 
+        length: 100, nullable: false, 
+        comment: "Dia da semana do horário de funcionamento." })
     dayOfWeek!: WeekDay;
 
-    @Column({ type: "time", nullable: false })
+    @Column({ 
+        type: "time", 
+        nullable: false, 
+        comment: "Hora de abertura do centro médico." })
     openTime!: string;
 
-    @Column({ type: "time", nullable: false })
+    @Column({ 
+        type: "time", 
+        nullable: false, 
+        comment: "Hora de fechamento do centro médico." })
     closeTime!: string;
 
-    @CreateDateColumn()
+    @CreateDateColumn({ comment: "Data de criação do registro." })
     createdAt!: Date;
 
-    @UpdateDateColumn()
-    updatedAt!: Date
+    @UpdateDateColumn({ comment: "Data da última atualização do registro." })
+    updatedAt!: Date;
 
-    @DeleteDateColumn()
-    deletedAt!: Date
+    @DeleteDateColumn({ comment: "Data de exclusão." })
+    deletedAt!: Date;
 }
