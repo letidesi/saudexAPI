1fdad2052b972aea2aae9835a4ce04d27286f59a
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-11-15 13:32:29 -0300

    refactor(config): add new config to mongo

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,17 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export const config = {
   server: {
-    port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT,
+    env: process.env.NODE_ENV,
   },
 
-  database: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 1433,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    name: process.env.DB_NAME,
-    encrypt: true,
-    trustServerCertificate: true,
+  mongo: {
+    uri: process.env.MONGO_URI || "",
   },
 
   swagger: {
-    serverUrl: process.env.SWAGGER_SERVER_URL || 'http://localhost:3000/api',
-    hideSensitive: process.env.SWAGGER_HIDE_SENSITIVE === 'true',
+    serverUrl: process.env.SWAGGER_SERVER_URL,
+    hideSensitive: process.env.SWAGGER_HIDE_SENSITIVE === "true",
   },
 };
